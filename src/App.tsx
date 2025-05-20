f5d7dac7a81841068bc747177e2f04f8c7586906
orisimh <zori1232@gmail.com>
2025-05-20 18:59:32 +0300

    routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css';
+import React from 'react';
 
 import DynamicForm from './components/Common/DynamicForm';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 
 function App() {
   
 
   return (
-    <div className="app">
-      <h1>Create New</h1>
-      { <DynamicForm  />}
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Request Form</h1>
+        <Routes>
+          <Route path="/" element={<Navigate to="/create" replace />} />
+          <Route path="/create" element={<DynamicForm />} />        </Routes>
+      </div>
+    </Router>
   );
 }
 
